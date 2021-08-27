724ccf17c8471e6207338ca47dce69898d59b732
Bastianderson <Redoreole@gmail.com>
2021-08-27 20:22:38 +0300

    Наращивание базового функционала - пока по лекциям. Плюс прототип Watcher'a

--- Storage_Netty_L3/client/src/main/java/com/geekbrains/storage/client/Client.java
+++ Storage_Netty_L3/client/src/main/java/com/geekbrains/storage/client/Client.java
@@ -5,9 +5,13 @@ import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.bytes.ByteArrayEncoder;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 public class Client {
@@ -24,7 +28,7 @@ public class Client {
                     .handler(new ChannelInitializer<NioSocketChannel>() {
                         @Override
                         protected void initChannel(NioSocketChannel ch) throws Exception {
-                            ch.pipeline().addLast(new StringEncoder(), new StringDecoder(), new ClientDecoder());
+                            ch.pipeline().addLast(new ByteArrayEncoder(), new StringDecoder(), new ClientDecoder());
                         }
                     });
 
@@ -32,8 +36,19 @@ public class Client {
 
             System.out.println("Client has started...");
             while (true) {
-                String msg = String.format("Hello from client %s: %s", Thread.currentThread().getName(), LocalDateTime.now());
-                channelFuture.channel().writeAndFlush(msg).sync();
+                //String msg = String.format("Hello from client %s: %s", Thread.currentThread().getName(), LocalDateTime.now());
+                try (FileInputStream fileInputStream = new FileInputStream("text.txt")) {
+                    BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+                    if (bufferedInputStream.available() > 1024) {
+                        byte[] fileBytes = new byte[1024];
+                        bufferedInputStream.read(fileBytes);
+                        channelFuture.channel().writeAndFlush(fileBytes).sync();
+                    } else {
+                        byte[] fileBytes = new byte[fileInputStream.available()];
+                        bufferedInputStream.read(fileBytes);
+                        channelFuture.channel().writeAndFlush(fileBytes).sync();
+                    }
+                } catch (IOException e) { }
 
                 try {
                     Thread.sleep(1000);
